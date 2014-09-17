ef7cf4415d952736a04ef6f9948eafc513528d08
anna mendoza <anna@curvecommunications.biz>
2014-09-17 12:06:14 -0700

    updated to both the layouts and how the mixins will be working inside the project

--- public/javascripts/main_squeeze.js
+++ public/javascripts/main_squeeze.js
@@ -1,3 +1,22 @@
-  var paper = new Raphael(document.getElementById('contained'), 900, 600);
+//The First Middleware Animation:
+  var paper = new Raphael(document.getElementById('client-side'), 262.5, 610);
   var pearl = paper.circle(125, 100, 70); // starting point circle
 	pearl.attr("fill", "black");
+	
+	
+	var paper = new Raphael(document.getElementById('middleware-1'), 165, 610);
+  var pearl = paper.circle(125, 100, 70); // starting point circle
+	pearl.attr("fill", "black");
+	
+	var paper = new Raphael(document.getElementById('middleware-2'), 165, 610);
+  var pearl = paper.circle(125, 100, 70); // starting point circle
+	pearl.attr("fill", "black");
+	
+  var paper = new Raphael(document.getElementById('middleware-3'), 165, 610);
+  var pearl = paper.circle(125, 100, 70); // starting point circle
+	pearl.attr("fill", "black");
+	
+	var paper = new Raphael(document.getElementById('rackrails'), 262.5, 610);
+  var pearl = paper.circle(125, 100, 70); // starting point circle
+	pearl.attr("fill", "black");
+
